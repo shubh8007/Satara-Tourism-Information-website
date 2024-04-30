174072b08dbbac04f8e64af48e5d67a0b702a8fc
shubham suryawanshi <shubhams0237@gmail.com>
2024-04-30 08:49:12 +0530

    Sucess

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Example of dynamic content
+document.getElementById('destinations').addEventListener('click', function() {
+    alert('You clicked on Destinations!');
+});
+
+// Add more JavaScript functionalities as needed
